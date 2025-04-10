bc342f858892de3d5b97c8742f53add993dee806
Robinhood0123 <93657599+Robinhood0123@users.noreply.github.com>
2025-04-10 13:52:49 +0200

    2

---
+++ szerver.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    port: '3306',
+    password: '',
+    database: 'fogado',
+});
+
+app.get('/', (req, res) => {
+    res.send('DOLGOZAT');
+});
+
+app.listen(3001, () => {
+    console.log('fut a 3001-en');
+});
+
+app.get/("/szobak", (req, res) => {
+    const sql = "SELECT * FROM szobak";
+    db.query(sql, (err, result) => {
+        if (err) return res.tatus(500).json({error: err.message})
+        res.json(results);
+    })
+})
