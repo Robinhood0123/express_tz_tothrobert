559f3556cfa41c589532c86d0646833b3805d651
Robinhood0123 <93657599+Robinhood0123@users.noreply.github.com>
2025-04-10 14:05:27 +0200

    4

--- szerver.js
+++ szerver.js
@@ -1,32 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
+const mysql = require("mysql");
+const cors = require("cors");
+ 
 app.use(cors());
-app.use(bodyParser.json());
-
+ 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    port: '3306',
-    password: '',
-    database: 'fogado',
+    user: "root",
+    host: "localhost",
+    port: 3306,
+    password: "",
+    database: "fogado"
+})
+ 
+app.get("/", (req, res) => {
+    res.send("Fut a backend.");
 });
-
-app.get('/', (req, res) => {
-    res.send('A szerver működik!');
-});
-
-app.get/("/szobak", (req, res) => {
+ 
+app.get("/szobak", (req, res) => {
     const sql = "SELECT * FROM szobak";
     db.query(sql, (err, result) => {
-        if (err) return res.tatus(500).json({error: err.message})
-        res.json(results);
-    })
-})
-
-app.listen(3001, () => {
-    console.log('fut a 3001-en');
+        res.json(result);
+    });
 });
+   
+app.listen(3001, () => {
+    console.log("Szerver a 3001-es porton fut.");
+});
+ 
