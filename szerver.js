439abec41eab9212959d1c15fe207de0bd6b50d1
Robinhood0123 <93657599+Robinhood0123@users.noreply.github.com>
2025-04-10 14:06:58 +0200

    4

--- szerver.js
+++ szerver.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
-const cors = require("cors");
- 
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 app.use(cors());
- 
+app.use(bodyParser.json());
+
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    port: 3306,
-    password: "",
-    database: "fogado"
-})
- 
+    user: 'root',
+    host: 'localhost',
+    port: '3306',
+    password: '',
+    database: 'fogado',
+});
+
 app.get("/", (req, res) => {
     res.send("Fut a backend.");
 });
@@ -27,4 +29,3 @@ app.get("/szobak", (req, res) => {
 app.listen(3001, () => {
     console.log("Szerver a 3001-es porton fut.");
 });
- 
